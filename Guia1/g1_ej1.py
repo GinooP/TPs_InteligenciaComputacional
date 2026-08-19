93b6d139a979fc4fc7a2b820e4d3352411dd1761
SantiFFF <santiagofaule@gmail.com>
2026-08-19 15:11:32 -0300

    ej1 original

--- Guia1/g1_ej1.py
+++ Guia1/g1_ej1.py
@@ -1,83 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import trn_perceptron as trn
 
-datos_trn= np.loadtxt("Guia1 auxiliar/OR_trn.csv",delimiter=',',skiprows=0) #leemos los datos del archivo de texto
-#print(datos_trn)
+# Etapa de Entrenamiento
+datos = np.loadtxt('Guia1/OR_trn.csv', delimiter=',', skiprows=0)
+tasa_de_aprendizaje = 0.5
+maxit = 1
+w, ratio, matriz_w = trn.trn_perceptron(datos, tasa_de_aprendizaje, maxit)
 
-M=len(datos_trn[:,0]) #obtenemos la cantidad de patrones, osea datos
-N=len(datos_trn[0,:]) #obtenemos la cantidad de entradas (las columnas)
+print(f"El ratio de aciertos es de: {ratio*100} %")
 
-#agregamos la entrada del bias (la del x0)
-entradas_trn= -1*np.ones((M,N)) 
-entradas_trn[:,1:N]= datos_trn[:,0:N-1] #agregamos todas las entradas sin poner las salidas (la ultima fila)
+# Etapa de Validacion
 
-#sacamos las salidas(no hace falta pero bueeeenooo)
-salidas_trn=datos_trn[:,N-1]
+datos_tst = np.loadtxt('Guia1/OR_tst.csv', delimiter=',', skiprows=0)
 
-#inicializamos los pesos de manera aleatoria entre -0.5 y 0.5
+N = len(datos_tst[:,0]) # Cantidad de patrones (pruebas)
+M = len(datos_tst[0,:]) # Cantidad de entradas +
 
-rng= np.random.default_rng()
-w= rng.random(N) - 0.5 #creamos un vector de pesos sinapticos de tamaño igual a la cantidad de las entradas
+y_d_tst = datos_tst[:,M-1] # Vector con salidas esperadas
 
-#etapa de aprendisaje
-coef_aprendisaje = 0.2 #usar valores bajos
-maxit=200 #cantidad maxima de iteraciones
-it_utilizadas=0
-for it in range(maxit):
-    for i in range(M):
-        #hacemos el producto punto entre los pesos sinapticos y la salida
-        #al producto lo metemos como entrada en la funcion de activacion (en este caso usamos la funcion signo)
-        y = np.sign(np.dot(w,entradas_trn[i,:])) #calculamos la salida
-    
-        #ahora actualizamos los pesos usando la formula
-        w = w + 0.5*coef_aprendisaje*(salidas_trn[i] - y)*entradas_trn[i,:]
+x_tst = -1 * np.ones((N,M))
+x_tst[:,1:M] = datos_tst[:,0:M-1]
 
-    #etapa de verificacion
-    aciertos=0
-    for i in range(M):
-        y=np.sign(np.dot(w,entradas_trn[i,:]))
-        
-        if ((salidas_trn[i] - y) == 0):
-            aciertos += 1
-    ratio_trn=aciertos/M
-    print(f"el ratio de practica obtenido en la iteracion {it} es {ratio_trn*100}")
-    if (ratio_trn == 1):
-        it_utilizadas=it
-        break
+aciertos_tst = 0
+for j in range(N):
+    y = np.sign(np.dot(w,x_tst[j,:]))
+    if (y_d_tst[j] - y) == 0:
+        aciertos_tst += 1
+ratio_tst = aciertos_tst / N
 
-if(it_utilizadas!=0):
-    print(f"se requirieron {it_utilizadas} iteraciones para el aprendizaje")
-else:
-    print(f"se requierieron {maxit} iteraciones para el aprendizaje")
-
-
-
-#ahora hacemos las pruebas
-datos_tst= np.loadtxt("Guia1 auxiliar/OR_tst.csv",delimiter=',',skiprows=0) #leemos los datos del test
-
-M=len(datos_tst[:,0])# cantidad de patrones para el test
-N=len(datos_tst[0,:])#cantidad de entradas para el test
-
-entradas_tst= -1*np.ones((M,N))
-entradas_tst[:,1:N]=datos_tst[:,0:N-1]
-
-salidas_tst=datos_tst[:,N-1]
-
-aciertos=0
-for i in range(M):
-    #calculamos las salidas dejando los ultimos pesos sinapticos obtenidos
-    yd= np.sign(np.dot(w,entradas_tst[i,:]))
-
-    if ((salidas_tst[i] - yd) == 0):
-        aciertos += 1
-
-#obtenemos el ration de aciertos (aciertos / cantidad de datos del test)
-ratio= aciertos/M
-print(f"El ratio de aciertos del test es de: {ratio*100} %")
-
-########## Graficamos ###########################
-
-x2_frontera = w[0]/w[2] - (w[1]/w[2])*entradas_tst[:,1]
-plt.scatter(entradas_tst[:,2],entradas_tst[:,1])
-plt.plot(x2_frontera,entradas_tst[:,1],color='red')
-plt.show()
+print(f"El ratio de aciertos del test es de: {ratio_tst*100} %")
