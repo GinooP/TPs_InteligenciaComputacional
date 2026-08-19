594d83020b5dfcd06c513f88f9692b8e42d70755
SantiFFF <santiagofaule@gmail.com>
2026-08-19 16:32:11 -0300

    trn ahora actualiza los pesos en la matriz solo si cambian, y solucione el problema de los graficos del ejercicio 2

--- Guia1/g1_ej2.py
+++ Guia1/g1_ej2.py
@@ -4,8 +4,8 @@ import time
 import trn_perceptron as trn
 import tst_perceptron as tst
 
-datos_OR_trn=np.loadtxt("Guia1 auxiliar/OR_trn.csv",delimiter=',',skiprows=0)
-datos_XOR_trn=np.loadtxt("Guia1 auxiliar/XOR_trn.csv",delimiter=',',skiprows=0)
+datos_OR_trn=np.loadtxt("Guia1/OR_trn.csv",delimiter=',',skiprows=0)
+datos_XOR_trn=np.loadtxt("Guia1/XOR_trn.csv",delimiter=',',skiprows=0)
 
 #obtenemos los pesos sinapticos y el historial de pesos sinapticos
 
@@ -17,41 +17,16 @@ w_XOR, ratio_trn_XOR, historial_w_XOR = trn.trn_perceptron(datos_XOR_trn,tasa_de
 
 #testeamos con nuevos patrones
 
-datos_OR_tst=np.loadtxt("Guia1 auxiliar/OR_tst.csv",delimiter=',',skiprows=0)
-datos_XOR_tst=np.loadtxt("Guia1 auxiliar/XOR_tst.csv",delimiter=',',skiprows=0)
+datos_OR_tst=np.loadtxt("Guia1/OR_tst.csv",delimiter=',',skiprows=0)
+datos_XOR_tst=np.loadtxt("Guia1/XOR_tst.csv",delimiter=',',skiprows=0)
 
 # ratio_tst_OR= tst.tst_perceptron(datos_OR_tst,w_OR)
 # ratio_tst_XOR= tst.tst_perceptron(datos_XOR_tst,w_XOR)
 
-#### Graficamos las distintas fronteras del XOR
-M=len(datos_XOR_tst[0,:])#cantidad de entradas para el test
-N=len(datos_XOR_tst[0,:])#cantidad de entradas para el test
-
-
-
-
-
-cant_pesos = len(historial_w_XOR[:,0])
-yd_XOR=datos_XOR_tst[:,-1]# salida esperada XOR
-plt.figure(1)
-plt.ion()
-fig, ax = plt.subplots()
-
-for i in range(cant_pesos):
-    x2_frontera = historial_w_XOR[i,0]/historial_w_XOR[i,2] - (historial_w_XOR[i,1]/historial_w_XOR[i,2])*datos_XOR_tst[:,0]
-    ax.clear()
-    ax.scatter(datos_XOR_tst[:,1],datos_XOR_tst[:,0],c=yd_XOR,cmap='bwr')
-    ax.plot(x2_frontera,datos_XOR_tst[:,0],color='green')
-    ax.set_xlim(-1.5, 1.5) 
-    ax.set_ylim(-1.5, 1.5)
-    plt.pause(0.3)
-
-plt.ioff()
-
 ######### graficamos OR ##########################
-plt.figure(2)
 cant_pesos = len(historial_w_OR[:,0])
 yd_OR=datos_OR_tst[:,-1]# salida esperada or
+
 plt.ion()
 fig2, ax2 = plt.subplots()
 
@@ -62,9 +37,28 @@ for i in range(cant_pesos):
     ax2.plot(x2_frontera,datos_OR_tst[:,0],color='green')
     ax2.set_xlim(-1.5, 1.5) 
     ax2.set_ylim(-1.5, 1.5)
+    ax2.set_title("OR")
     plt.pause(0.3)
 
 plt.ioff()
 
+
+cant_pesos = len(historial_w_XOR[:,0])
+yd_XOR=datos_XOR_tst[:,-1]# salida esperada XOR
+
+plt.ion()
+fig, ax = plt.subplots()
+
+for i in range(cant_pesos):
+    x2_frontera = historial_w_XOR[i,0]/historial_w_XOR[i,2] - (historial_w_XOR[i,1]/historial_w_XOR[i,2])*datos_XOR_tst[:,0]
+    ax.clear()
+    ax.scatter(datos_XOR_tst[:,1],datos_XOR_tst[:,0],c=yd_XOR,cmap='bwr')
+    ax.plot(x2_frontera,datos_XOR_tst[:,0],color='green')
+    ax.set_xlim(-1.5, 1.5) 
+    ax.set_ylim(-1.5, 1.5)
+    ax.set_title("XOR")
+    plt.pause(0.3)
+
+plt.ioff()
 
 plt.show()
--- Guia1/trn_perceptron.py
+++ Guia1/trn_perceptron.py
@@ -20,7 +20,8 @@ def trn_perceptron(datos, tasa_de_aprendizaje, maxit):
         for j in range(N):
             y = np.sign(np.dot(w,x[j,:]))
             w = w + tasa_de_aprendizaje/2 * (y_d[j] - y)*x[j,:]
-            historial_w.append(np.copy(w))
+            if ((y_d[j] - y) != 0):
+                historial_w.append(np.copy(w))
 
         # Etapa de ratio de aciertos
         aciertos = 0
